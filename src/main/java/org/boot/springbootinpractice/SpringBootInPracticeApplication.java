199bdeec444ad9e68121853daf551d772f029800
2hongbi <foxgirin950@gmail.com>
2024-06-25 02:28:29 +0900

    Update SpringBootInPracticeApplication with new configuration settings

--- src/main/java/org/boot/springbootinpractice/SpringBootInPracticeApplication.java
+++ src/main/java/org/boot/springbootinpractice/SpringBootInPracticeApplication.java
@@ -1,5 +1,7 @@
 package org.boot.springbootinpractice;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
@@ -18,94 +20,4 @@ public class SpringBootInPracticeApplication {
         SpringApplication.run(SpringBootInPracticeApplication.class, args);
     }
 
-}
-
-
-class Coffee {
-    private final String id;
-    private String name;
-
-    public Coffee(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Coffee(String name) {
-        this(UUID.randomUUID().toString(), name);
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
-
-
-@RestController
-@RequestMapping("/coffees")
-class RestApiDemoController {
-    private List<Coffee> coffees = new ArrayList<>();
-
-
-    public RestApiDemoController() {
-        coffees.addAll(List.of(
-           new Coffee("Cafe Cereza"),
-           new Coffee("Cafe Ganador"),
-           new Coffee("Cafe Lareno"),
-           new Coffee("Cafe Tres Pontas")
-        ));
-    }
-
-    @GetMapping
-    Iterable<Coffee> getCoffees() {
-        return coffees;
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Coffee> getCoffeeById(@PathVariable String id) {
-        Optional<Coffee> coffee = coffees.stream()
-                .filter(c -> c.getId().equals(id))
-                .findFirst();
-        return coffee.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
-
-    @PostMapping
-    Coffee postCoffee(@RequestBody Coffee coffee) {
-        coffees.add(coffee);
-        return coffee;
-    }
-
-    @PutMapping("/{id}")
-    ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-        int coffeeIndex = -1;
-
-        for (Coffee c : coffees) {
-            if (c.getId().equals(id)) {
-                coffeeIndex = coffees.indexOf(c);
-                coffees.set(coffeeIndex, coffee);
-            }
-        }
-
-        return (coffeeIndex == -1) ?
-                new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
-                new ResponseEntity<>(coffee, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    void deleteCoffee(@PathVariable String id) {
-        coffees.removeIf(c -> c.getId().equals(id));
-    }
-
-    @GetMapping("/")
-    public String home() {
-        return "Welcome to the Coffee API!";
-    }
 }
