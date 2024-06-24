c4156188520c43b786b08a1c027d31b0f1d09099
2hongbi <foxgirin950@gmail.com>
2024-06-25 02:46:50 +0900

    Create CoffeeRepository interface extending CrudRepository

--- src/main/java/org/boot/springbootinpractice/domain/CoffeeRepository.java
+++ src/main/java/org/boot/springbootinpractice/domain/CoffeeRepository.java
@@ -1,5 +1,6 @@
-package org.boot.springbootinpractice;
+package org.boot.springbootinpractice.domain;
 
+import org.boot.springbootinpractice.domain.Coffee;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CoffeeRepository extends CrudRepository<Coffee, String> { }
